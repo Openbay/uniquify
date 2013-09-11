94b956b86a6182f679115fc20aedc71bf327961e
Adam Bair <adambair@gmail.com>
2013-09-11 14:48:56 -0400

    Rails and not Rails. Let's find out.

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -3,12 +3,6 @@ module Uniquify
     base.extend ClassMethods
   end
 
-  def ensure_unique(name)
-    begin
-      self[name] = yield
-    end while self.class.exists?(name => self[name])
-  end
-
   module ClassMethods
     def default_options
       { :length => 8, 
@@ -21,27 +15,47 @@ module Uniquify
       options[:chars].reject!{|char| options[:omit].include?(char)}
       Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
     end
+  end
+end
 
-    def uniquify(*args, &block)
-      options = default_options
-      options.merge!(args.pop) if args.last.kind_of? Hash
-      options[:chars].reject!{|char| options[:omit].include?(char)}
-      args.each do |name|
-        before_validation :on => :create do |record|
-          if block
-            record.ensure_unique(name, &block)
-          else
-            record.ensure_unique(name) do
-              Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
+module Uniquify
+  module Rails
+    def self.included(base)
+      base.extend ClassMethods
+      base.extend RailsClassMethods
+    end
+
+    def ensure_unique(name)
+      begin
+        self[name] = yield
+      end while self.class.exists?(name => self[name])
+    end
+
+    module ClassMethods
+      def uniquify(*args, &block)
+        options = default_options
+        options.merge!(args.pop) if args.last.kind_of? Hash
+        options[:chars].reject!{|char| options[:omit].include?(char)}
+        args.each do |name|
+          before_validation :on => :create do |record|
+            if block
+              record.ensure_unique(name, &block)
+            else
+              record.ensure_unique(name) do
+                Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
+              end
             end
           end
         end
       end
     end
-
   end
 end
 
-class ActiveRecord::Base
+class Uniquify
   include Uniquify
+end
+
+class ActiveRecord::Base
+  include Uniquify::Rails
 end
