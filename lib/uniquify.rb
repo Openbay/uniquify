75f5e8b9bb005f6ad57fe5e517f29b11b7b373b9
Adam Bair <adambair@gmail.com>
2013-09-11 14:29:52 -0400

    Add generate_code

--- lib/uniquify.rb
+++ lib/uniquify.rb
@@ -10,10 +10,22 @@ module Uniquify
   end
 
   module ClassMethods
+    def default_options
+      { :length => 8, 
+        :chars => ('a'..'z').to_a + ('0'..'9').to_a, 
+        :omit => [] }
+    end
+
+    def generate_code(opts={})
+      options = default_options.merge(opts)
+      options[:chars].reject!{|char| options[:omit].include?(char)}
+      Array.new(options[:length]) { options[:chars].to_a[rand(options[:chars].to_a.size)] }.join
+    end
 
     def uniquify(*args, &block)
-      options = { :length => 8, :chars => ('a'..'z').to_a + ('0'..'9').to_a }
+      options = default_options
       options.merge!(args.pop) if args.last.kind_of? Hash
+      options[:chars].reject!{|char| options[:omit].include?(char)}
       args.each do |name|
         before_validation :on => :create do |record|
           if block
